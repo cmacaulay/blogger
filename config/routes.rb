c96279c545294c4051f2075522450924d1c8632e
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-02-05 19:03:49 -0700

    finish blog comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # Have to have the routes defined, you can't just define them once and delete
 
 end
